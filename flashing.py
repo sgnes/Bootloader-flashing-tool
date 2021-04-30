13d0581b17e568b1877241f262ffb42c0357eba0
sgnes <sgnes0514@gmail.com>
2021-04-30 21:12:20 +0800

    add parameters

--- flashing.py
+++ flashing.py
@@ -12,6 +12,8 @@ import logging
 import os
 import json
 from udsoncan import Request, MemoryLocation
+import argparse
+
 
 def setup_logging(default_path='logging.json', default_level=logging.DEBUG, env_key='LOG_CFG'):
    """Setup logging configuration
@@ -49,48 +51,64 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     if iteration == total:
         print()
 
-setup_logging()
+def reprogramming(conn, args):
+   setup_logging()
 
-src = Srecords("MASTER_A003.hex")
-#print(src.get_data_by_addr(0x4c00, 0x7000))
+   src = Srecords(args.file)
+   seq = 1
+   transfer_cnt = 0
+   max_data_per_seq = 0x100
+   with Client(conn, request_timeout=5, config=client_config) as client:                                     # Application layer (UDS protocol)
+      response = client.change_session(3)
+      print(binascii.hexlify(response.data))
+      response = client.change_session(2)
+      print(binascii.hexlify(response.data))
+      response = client.unlock_security_access(0x11)
+      print(binascii.hexlify(response.data))
+      response = client.start_routine(0xff00, bytes([1, 0]))
+      print(binascii.hexlify(response.data))
+      memloc1 = MemoryLocation(address=args.app_start_addr, memorysize=args.app_size, address_format=24, memorysize_format=24)
+      response = client.request_download(memloc1)
+      print(binascii.hexlify(response.data))
+      current_addr = args.app_start_addr
 
+      printProgressBar(0, args.app_size, prefix='Progress:', suffix='Complete', length=50)
 
-bus = PcanBus()                                          # Link Layer (CAN protocol)
-tp_addr = isotp.Address(isotp.AddressingMode.Normal_11bits, txid=project_config.uds_req_phy_id, rxid=project_config.uds_res_id) # Network layer addressing scheme
-stack = isotp.CanStack(bus=bus, address=tp_addr, params=isotp_params)               # Network/Transport layer (IsoTP protocol)
-conn = PythonIsoTpConnection(stack)                                                 # interface between Application and Transport layer
-seq = 1
-transfer_cnt = 0
-max_data_per_seq = 0x100
-with Client(conn, request_timeout=5, config=client_config) as client:                                     # Application layer (UDS protocol)
-   response = client.change_session(3)
-   print(binascii.hexlify(response.data))
-   response = client.change_session(2)
-   print(binascii.hexlify(response.data))
-   response = client.unlock_security_access(0x11)
-   print(binascii.hexlify(response.data))
-   response = client.start_routine(0xff00, bytes([1, 0]))
-   print(binascii.hexlify(response.data))
-   memloc1 = MemoryLocation(address=project_config.app_start_addr, memorysize=project_config.app_size, address_format=24, memorysize_format=24)
-   response = client.request_download(memloc1)
-   print(binascii.hexlify(response.data))
-   current_addr = project_config.app_start_addr
+      while transfer_cnt < args.app_size:
+         cnt = min(max_data_per_seq, args.app_size-transfer_cnt)
+         data = src.get_data_by_addr(current_addr, cnt)
+         response = client.transfer_data(seq,  bytes.fromhex(data))
+         seq += 1
+         current_addr += cnt
+         transfer_cnt +=  cnt
+         printProgressBar(transfer_cnt, args.app_size, prefix='Progress:', suffix='Complete', length=50)
 
-   printProgressBar(0, project_config.app_size, prefix='Progress:', suffix='Complete', length=50)
+      client.request_transfer_exit()
+      response = client.start_routine(0x0202, bytes([3, 0, 0x4c, 0]))
+      print(binascii.hexlify(response.data))
+      response = client.start_routine(0xff01)
+      print(binascii.hexlify(response.data))
+      response = client.ecu_reset(1)
+      print(binascii.hexlify(response.data))
 
-   while transfer_cnt < project_config.app_size:
-      cnt = min(max_data_per_seq, project_config.app_size-transfer_cnt)
-      data = src.get_data_by_addr(current_addr, cnt)
-      response = client.transfer_data(seq,  bytes.fromhex(data))
-      seq += 1
-      current_addr += cnt
-      transfer_cnt +=  cnt
-      printProgressBar(transfer_cnt, project_config.app_size, prefix='Progress:', suffix='Complete', length=50)
+if __name__ == '__main__':
+   parser = argparse.ArgumentParser(description='test')
 
-   client.request_transfer_exit()
-   response = client.start_routine(0x0202, bytes([3, 0, 0x4c, 0]))
-   print(binascii.hexlify(response.data))
-   response = client.start_routine(0xff01)
-   print(binascii.hexlify(response.data))
-   response = client.ecu_reset(1)
-   print(binascii.hexlify(response.data))
+   parser.add_argument('--can_interface', default='Vector', help='The CAN interface used for programming, PCAN, Vector...')
+   parser.add_argument('--can_channel', default=0, help='The CAN interface channel used for programming, 0,1.')
+   parser.add_argument('--baud_rate', type=int, default=500, help='CAN baud rate.')
+   parser.add_argument('--file', type=str, default="MASTER_A003.hex", help='The file to be reprogrammed')
+   parser.add_argument('--app_start_addr', type=int, default=0x4c00, help='The start address of app')
+   parser.add_argument('--app_size', type=int, default=0x700, help='The size of app')
+
+   args = parser.parse_args()
+
+   if args.can_interface == "Vector":
+      bus = VectorBus(args.can_channel)
+   elif args.can_interface == "PCAN":
+      bus = PcanBus()  # Link Layer (CAN protocol)
+   tp_addr = isotp.Address(isotp.AddressingMode.Normal_11bits, txid=project_config.uds_req_phy_id,
+                           rxid=project_config.uds_res_id)  # Network layer addressing scheme
+   stack = isotp.CanStack(bus=bus, address=tp_addr, params=isotp_params)  # Network/Transport layer (IsoTP protocol)
+   conn = PythonIsoTpConnection(stack)
+   reprogramming(conn, args)
